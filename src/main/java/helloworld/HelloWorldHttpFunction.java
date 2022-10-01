070f207c11d32ddb2ca8cafb0ed2de4722643068
tmiki <sidhemail@gmail.com>
2022-10-01 13:15:19 +0900

    Added a code of Cloud Functions entry point.

--- src/main/java/helloworld/HelloWorldHttpFunction.java
+++ src/main/java/helloworld/HelloWorldHttpFunction.java
@@ -1,5 +1,17 @@
 package helloworld;
 
-public class HelloWorldHttpFunction {
-    
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class HelloWorldHttpFunction implements HttpFunction {
+  // Simple function to return "Hello World"
+  @Override
+  public void service(HttpRequest request, HttpResponse response)
+      throws IOException {
+    BufferedWriter writer = response.getWriter();
+    writer.write("Hello World!");
+  }
 }
